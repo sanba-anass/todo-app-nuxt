5ea7deb92dac8a0fad1d1b9b0ff878ee4bc33ffe
anasssanba1 <sanbaanass73@gmail.com>
2023-09-04 15:55:02 +0200

    adding netlify preset

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,11 +1,11 @@
-// export default defineNuxtRouteMiddleware((to, from) => {
-// 	const user = useSupabaseUser();
-// 	if (user._object.$ssupabase_user !== null && to.path !== "/") {
-// 		return navigateTo("/");
-// 	} else if (
-// 		user._object.$ssupabase_user === null &&
-// 		to.path !== "/account/login"
-// 	) {
-// 		return navigateTo("/account/login");
-// 	}
-// });
+export default defineNuxtRouteMiddleware((to, from) => {
+	const user = useSupabaseUser();
+	if (user._object.$ssupabase_user !== null && to.path !== "/") {
+		return navigateTo("/");
+	} else if (
+		user._object.$ssupabase_user === null &&
+		to.path !== "/account/login"
+	) {
+		return navigateTo("/account/login");
+	}
+});
